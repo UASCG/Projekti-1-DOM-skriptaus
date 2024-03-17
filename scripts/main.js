cf42e2d381f5d7b919da6dea3a8ef501468976f6
UASCG <christian.gestrin@student.laurea.fi>
2024-03-17 13:19:37 +0200

    Working delete from localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -3,46 +3,33 @@ var liAll = document.getElementsByTagName("LI");
 var ulList = document.querySelector("UL");
 const taskArray = [];
 
-// Creates a "delete" button and appends it to each list item.
-var i;
-for (i = 0; i < liAll.length; i++) {
-    var span = document.createElement("SPAN");
-    var xSign = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(xSign);
-    liAll[i].appendChild(span);
-}
+// Load tasks from localStorage.
+window.onload = loadTasks()
 
-// Clicking on a "delete" button deletes the list item.
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        console.log(i);
-        console.log(taskArray.indexOf(i));
-        console.log(myTasks.indexOf(i));
-        var div = this.parentElement;
-        const index = taskArray.indexOf([i]);
-        if (index > -1) { // only splice array when item is found
-            taskArray.splice(index, 1); // 2nd parameter means remove one item only
+function loadTasks() {
+    // Reads data from local storage and creates list items upon page load.
+    myTasks = JSON.parse(localStorage.getItem("myTasks"));
+
+    if (myTasks != null) { // Checks if data exists in local storage, i.e. key is not null.
+        console.log("Found " + myTasks.length + " saved tasks.")
+        for (let i = 0; i < myTasks.length; i++) {
+            console.log("myTasks: " + myTasks[i]);
+
+            // Creates new task from localStorage.
+            newTask(myTasks[i]);
+
+            // Creates "delete" button to newly added entry.
+            deleteTask(myTasks[i]);
         }
-        div.remove();
-        localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves updated tasks into local storage under key "myTasks".
+    } else {
+        console.log("No saved tasks found.")
     }
 }
-
-// Adds a "checked" symbol when clicking on a list item.
-ulList.addEventListener("click", function (event) { // Listens for click events.
-    if (event.target.tagName === "LI") { // Checks if <li> item is being clicked.
-        event.target.classList.toggle('checked'); // Toggles checkmark on <li> item.
-    }
-}, false);
 
 // Creates a new task item in the list when clicking "Lisää".
-function newTask() {
+function newTask(inputValue) {
     let text; // Used for handling messages to user.
     var input = document.getElementById("taskInput");
-    var inputValue = document.getElementById("taskInput").value; // Takes user's input value, i.e. task.
     input.style.backgroundColor = "white";
     input.style.outline = "none";
     if (inputValue.length < 1) { // Checks if input is less than 1 character in length, not allowed if true.
@@ -58,30 +45,10 @@ function newTask() {
         document.getElementById("taskList").appendChild(li);
 
         // Creates "delete" button to newly added entry.
-        var span = document.createElement("SPAN");
-        var xSign = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(xSign);
-        li.appendChild(span);
-
-        // Clicking on the "delete" button deletes the list item.
-        var i;
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function () {
-                console.log(i);
-                console.log(taskArray.indexOf(i));
-                console.log(myTasks.indexOf(i));
-                var div = this.parentElement;
-                const index = taskArray.indexOf([i]);
-                if (index > -1) { // only splice array when item is found
-                    taskArray.splice(index, 1); // 2nd parameter means remove one item only
-                }
-                div.remove();
-                localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves updated tasks into local storage under key "myTasks".
-            }
-        }
+        deleteTask(inputValue);
 
         taskArray.push(inputValue); // Adds task to task array.
+        console.log("Updated taskArray: " + taskArray);
 
         localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves created tasks into local storage under key "myTasks".
 
@@ -91,61 +58,73 @@ function newTask() {
     document.getElementById("addTaskMessage").innerHTML = text;
 }
 
-// Reads data from local storage and creates list items upon page load.
-myTasks = JSON.parse(localStorage.getItem("myTasks"));
-
-if (myTasks != null) { // Checks if data exists in local storage, i.e. key is not null.
-    for (let i = 0; i < myTasks.length; i++) {
-        console.log("Adding entry for \"" + myTasks[i] + "\" to taskArray...");
-        taskArray.push(myTasks[i]);
-        console.log(myTasks);
+// Create a "close" button and append it to each list item
+function deleteTask() {
+    var i;
+    for (i = 0; i < liAll.length; i++) {
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      liAll[i].appendChild(span);
+    }
+    
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        console.log(this.parentElement.textContent.slice(0, -2));
+        console.log("taskArray:" + taskArray);
+        console.log("myTasks: " + myTasks);
+        var itemToDelete = this.parentElement.textContent;
+        itemToDelete = itemToDelete.replace(/\u00D7/gi, '')
+        console.log("Item to be deleted (index): " + itemToDelete);
+        index = taskArray.indexOf(itemToDelete);
+        console.log("Item to be deleted (index): " + index);
+        if (index > -1) { // only splice array when item is found
+            taskArray.splice(index, 1); // 2nd parameter means remove one item only
+        }
+        localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves updated tasks into local storage under key "myTasks".
+        var div = this.parentElement;
+        div.remove();
+      }
     }
 }
 
-if (myTasks != null) { // Checks if data exists in local storage, i.e. key is not null.
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        console.log(`${key}: ${localStorage.getItem(key)}`);
-    }
-}
 
-if (myTasks != null) { // Checks if data exists in local storage, i.e. key is not null.
-    console.log("Found " + myTasks.length + " saved tasks.")
-    for (let i = 0; i < myTasks.length; i++) {
-        console.log("Creating task entry for \"" + myTasks[i] + "\"...");
-
-        var li = document.createElement("li");
-        var textNode = document.createTextNode(myTasks[i]);
-        li.appendChild(textNode);
-        document.getElementById("taskList").appendChild(li);
-
-        // Creates "delete" button to newly added entry.
+/* // Creates a "delete" button and appends it to each list item.
+function deleteTask(inputValue) {
+    var i = inputValue;
+    for (i = 0; i < liAll.length; i++) {
         var span = document.createElement("SPAN");
         var xSign = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(xSign);
-        li.appendChild(span);
-
-        // Clicking on the "delete" button deletes the list item.
-        var close = document.getElementsByClassName("close");
-        console.log(i);
-        console.log(myTasks[i]);
-        close[i].onclick = function () {
-            deleteIndex = myTasks.indexOf(myTasks[i]) - 1;
-            console.log("myTasks.indexOf(myTasks[i]) = " + myTasks.indexOf(myTasks[i]))
-            console.log("deleteIndex = " + deleteIndex)
-            console.log(i);
-            var div = this.parentElement;
-            div.remove();
-            taskArray.splice(taskArray[deleteIndex], 1);
-            console.log("Deleting " + taskArray[deleteIndex]);
-            console.log(taskArray);
-            localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves updated tasks into local storage under key "myTasks".
-        }
+        liAll[i].appendChild(span);
     }
-} else {
-    console.log("No saved tasks found.")
 }
+
+var close = document.getElementsByClassName("close");
+close[i].onclick = function deleteTaskButton(i) {
+    var div = this.parentElement;
+    const index = taskArray.indexOf(i);
+    if (index > -1) { // only splice array when item is found
+        taskArray.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    div.remove();
+    localStorage.setItem("myTasks", JSON.stringify(taskArray)); // Stringifies and saves updated tasks into local storage under key "myTasks".
+}
+
+// Clicking on a "delete" button deletes the list item. */
+
+
+// Adds a "checked" symbol when clicking on a list item.
+ulList.addEventListener("click", function (event) { // Listens for click events.
+    if (event.target.tagName === "LI") { // Checks if <li> item is being clicked.
+        event.target.classList.toggle('checked'); // Toggles checkmark on <li> item.
+    }
+}, false);
 
 function clearStorage() { // Clears local storage when button is pressed.
     localStorage.clear();
